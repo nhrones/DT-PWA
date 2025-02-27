11c06e63373ec1d6fd1ae1179468e7f66a1d3fa9
NDH <ndhrones@gmail.com>
2025-02-26 19:17:36 -0500

    r

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ document.title = thisSchema.dbKey
  */
 const appContext = {
    DEV: false,
-   LOCAL_DB: true,
+   LOCAL_DB: false,
    LocalDbURL: "http://localhost:9099/",
    RemoteDbURL: "https://dt-kv-rpc.deno.dev/",
    RpcURL: "SSERPC/kvRegistration",
@@ -43,13 +43,23 @@ document.title = thisSchema.dbKey
  * We pass in a dbSchema and an appContext
  * @returns TableComponent.KvCache
  */
-const table = document.getElementById("table-component").init(thisSchema, appContext)
+// const table = document.getElementById("table-component").init(thisSchema, appContext)
+
+// const REQUIRE_PIN = true
+
+// if (REQUIRE_PIN) {
+//    document.getElementById("pin-component").init(table.kvCache)
+// }
+
+const footer = document.getElementById("footer-component")
+const table = footer.init(thisSchema, appContext)
 
 const REQUIRE_PIN = true
 
 if (REQUIRE_PIN) {
-   document.getElementById("pin-component").init(table.kvCache)
+   document.getElementById("pin-component").init(table.kvCache.CTX)
 }
+
 
 //TODO Do a clean separation of Data-Provider and Web-Components
 //TODO Separate PIN-UI from the Table-UI --> use PIN only for KV-RPC
